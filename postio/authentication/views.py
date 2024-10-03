afe432897309139bc23050fc94c7738b7de09bb0
Kamil Handzlik <kamol0209@gmail.com>
2024-10-03 19:31:07 +0200

    Added ProfileView in authentication views - needs further debugging

--- postio/authentication/views.py
+++ postio/authentication/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from .forms import RegistrationForm
+from django.views.generic import CreateView, View
+from .forms import RegistrationForm, UserForm
 
 
 # Create your views here.
@@ -18,3 +19,39 @@ class CustomLoginView(LoginView):
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('homepage')
+
+class ProfileView(LoginRequiredMixin, View):
+    template_name = 'register/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        user_form = UserForm(instance=request.user)
+        password_form = PasswordChangeCustomForm(user=request.user)
+
+        return render(request, self.template_name, {
+            'user_form': user_form,
+            'password_form': password_form
+        })
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserForm(request.POST, instance=request.user)
+        password_form = PasswordChangeCustomForm(user=request.user, data=request.POST)
+
+        if 'username' in request.POST:
+            if user_form.is_valid():
+                user_form.save()
+                messages.success(request, 'Profil został zaktualizowany pomyślnie.')
+                return redirect('profile')
+
+        if 'old_password' in request.POST:
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Hasło zostało zmienione pomyślnie.')
+                return redirect('profile')
+            else:
+                messages.error(request, "Wystąpił błąd. Proszę spróbuj ponownie.")
+
+        return render(request, self.template_name, {
+            'user_form': user_form,
+            'password_form': password_form
+        })
