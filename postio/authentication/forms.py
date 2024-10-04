7c0642cc2c733b4c78fadf81b223c3cde45ff5cc
Kamil Handzlik <kamol0209@gmail.com>
2024-10-04 20:06:00 +0200

    Debugging

--- postio/authentication/forms.py
+++ postio/authentication/forms.py
@@ -109,3 +109,11 @@ class RegistrationForm(UserCreationForm):
                 code='password_mismatch'
             )
         return password2
+
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
--- postio/authentication/views.py
+++ postio/authentication/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, View
-from .forms import RegistrationForm, UserForm
+from .forms import RegistrationForm
 
 
 # Create your views here.
@@ -11,6 +11,9 @@ class RegistrationView(CreateView):
     form_class = RegistrationForm
     template_name = 'authentication/register.html'
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
 class CustomLoginView(LoginView):
@@ -20,38 +23,4 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('homepage')
 
-class ProfileView(LoginRequiredMixin, View):
-    template_name = 'register/profile.html'
 
-    def get(self, request, *args, **kwargs):
-        user_form = UserForm(instance=request.user)
-        password_form = PasswordChangeCustomForm(user=request.user)
-
-        return render(request, self.template_name, {
-            'user_form': user_form,
-            'password_form': password_form
-        })
-
-    def post(self, request, *args, **kwargs):
-        user_form = UserForm(request.POST, instance=request.user)
-        password_form = PasswordChangeCustomForm(user=request.user, data=request.POST)
-
-        if 'username' in request.POST:
-            if user_form.is_valid():
-                user_form.save()
-                messages.success(request, 'Profil został zaktualizowany pomyślnie.')
-                return redirect('profile')
-
-        if 'old_password' in request.POST:
-            if password_form.is_valid():
-                user = password_form.save()
-                update_session_auth_hash(request, user)
-                messages.success(request, 'Hasło zostało zmienione pomyślnie.')
-                return redirect('profile')
-            else:
-                messages.error(request, "Wystąpił błąd. Proszę spróbuj ponownie.")
-
-        return render(request, self.template_name, {
-            'user_form': user_form,
-            'password_form': password_form
-        })
